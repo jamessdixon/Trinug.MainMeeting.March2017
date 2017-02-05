8fe06e13733fa7c51d7fd4038af9c8fb65cbe888
Jamie Dixon <jamessdixon@gmail.com>
2017-02-05 10:34:10 -0500

    Changed naming.  Added Twitter project

---
+++ Example03/ChickenSoftware.TwitterAnalyzer.Solution/ChickenSoftware.TwitterAnalyzer/AnalyzerTwitterData/run.py
@@ -0,0 +1,6 @@
+﻿import os
+
+# read the queue message and write to stdout
+input = open(os.environ['input']).read()
+message = "Python script processed queue message '{0}'".format(input)
+print(message)
---
+++ Example03/ChickenSoftware.TwitterAnalyzer.Solution/ChickenSoftware.TwitterAnalyzer/TwitterAnalysisNotification/index.js
@@ -0,0 +1,4 @@
+﻿module.exports = function (context, myQueueItem) {
+    context.log('Node.js queue trigger function processed work item', myQueueItem);
+    context.done();
+};
