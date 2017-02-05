76a3c96e3c469d313a26633ede9ec86e06ba2775
Jamie Dixon <jamessdixon@gmail.com>
2017-02-05 09:29:19 -0500

    updated projects

---
+++ AzureFunctions/ChickenSoftware.AzureFunctions/httpGet/run.fsx
@@ -0,0 +1,8 @@
+﻿#r "System.Net.Http"
+
+open System.Net
+open System.Net.Http
+
+let Run(req: HttpRequestMessage) =
+    let currentDateTime = DateTime.Now.ToString()
+    req.CreateResponse(HttpStatusCode.OK, sprintf "{%s}" currentDateTime)
