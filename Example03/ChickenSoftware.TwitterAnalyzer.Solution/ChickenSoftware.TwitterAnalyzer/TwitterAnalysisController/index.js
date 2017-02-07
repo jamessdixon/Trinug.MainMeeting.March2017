d9e5c37f55dae045ed4cf7bf226083a2be319c86
Jamie Dixon <jamessdixon@gmail.com>
2017-02-07 11:50:55 -0500

    added condition with no data in tables yet

--- Example03/ChickenSoftware.TwitterAnalyzer.Solution/ChickenSoftware.TwitterAnalyzer/TwitterAnalysisController/index.js
+++ Example03/ChickenSoftware.TwitterAnalyzer.Solution/ChickenSoftware.TwitterAnalyzer/TwitterAnalysisController/index.js
@@ -1,36 +1,43 @@
 ﻿module.exports = function (context, sentimentTable) {
-    var lastRecord = sentimentTable[sentimentTable.length - 1]
-    var positiveTweets = sentimentTable.filter(function (x) { return x.id == lastRecord.id && x.sentiment == 'positive' });
-    var negativeTweets = sentimentTable.filter(function (x) { return x.id == lastRecord.id && x.sentiment == 'negative' });
-    var neutralTweets = sentimentTable.filter(function (x) { return x.id == lastRecord.id && x.sentiment == 'neutral' });
-
     var body = "<!DOCTYPE html> <html> <head> </head> <body> ";
-    body += "<b>Overall sentiment score = " + lastRecord.score + "</b>";
-    body += "<br>"
-    body += "<b>Positive Tweets</b>";
-    body += "<br>"
-    for (var t in positiveTweets) {
-        body += positiveTweets[t].user;
-        body += " - ";
-        body += positiveTweets[t].text;
-        body += "<br>";
+
+    if (sentimentTable.length > 0) {
+        var lastRecord = sentimentTable[sentimentTable.length - 1]
+        var positiveTweets = sentimentTable.filter(function (x) { return x.id == lastRecord.id && x.sentiment == 'positive' });
+        var negativeTweets = sentimentTable.filter(function (x) { return x.id == lastRecord.id && x.sentiment == 'negative' });
+        var neutralTweets = sentimentTable.filter(function (x) { return x.id == lastRecord.id && x.sentiment == 'neutral' });
+
+        body += "<b>Overall sentiment score = " + lastRecord.score + "</b>";
+        body += "<br>"
+        body += "<b>Positive Tweets</b>";
+        body += "<br>"
+        for (var t in positiveTweets) {
+            body += positiveTweets[t].user;
+            body += " - ";
+            body += positiveTweets[t].text;
+            body += "<br>";
+        }
+        body += "<b>Negative Tweets</b>";
+        body += "<br>"
+        for (var t in negativeTweets) {
+            body += negativeTweets[t].user;
+            body += " - ";
+            body += negativeTweets[t].text;
+            body += "<br>";
+        }
+        body += "<b>Neutral Tweets</b>";
+        body += "<br>"
+        for (var t in neutralTweets) {
+            body += neutralTweets[t].user;
+            body += " - ";
+            body += neutralTweets[t].text;
+            body += "<br>";
+        }
     }
-    body += "<b>Negative Tweets</b>";
-    body += "<br>"
-    for (var t in negativeTweets) {
-        body += negativeTweets[t].user;
-        body += " - ";
-        body += negativeTweets[t].text;
-        body += "<br>";
+    else {
+        body += "No Analysis Yet...";
     }
-    body += "<b>Neutral Tweets</b>";
-    body += "<br>"
-    for (var t in neutralTweets) {
-        body += neutralTweets[t].user;
-        body += " - ";
-        body += neutralTweets[t].text;
-        body += "<br>";
-    }
+
     body += " </body> </html>";
 
     context.res = {
