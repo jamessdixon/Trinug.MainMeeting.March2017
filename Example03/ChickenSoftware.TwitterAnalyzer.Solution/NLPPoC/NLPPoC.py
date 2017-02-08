21d915aa08eddb86adb50c12e045564f8b795d40
Jamie Dixon <jamessdixon@gmail.com>
2017-02-08 07:16:06 -0500

    clean up

--- Example03/ChickenSoftware.TwitterAnalyzer.Solution/NLPPoC/NLPPoC.py
+++ Example03/ChickenSoftware.TwitterAnalyzer.Solution/NLPPoC/NLPPoC.py
@@ -22,8 +22,34 @@ def getSentimentScore(text):
         return 50.0
     return
 
-print(getSentimentScore('This is awesome'))
-print(getSentimentScore('This sucks'))
+def createSentiment(tweetLite):
+
+
+#{
+#    "id": "1",
+#    "score": "8.6",
+#    "sentiment": "positive",
+#    "rank": "1",
+#    "user": "Greg",
+#    "text": "#TRINUG Rulz"
+#},
+
+#print(getSentimentScore('This is awesome'))
+#print(getSentimentScore('This sucks'))
+
+
+
+json_data = open('TweetLite.json').read()
+data = json.loads(json_data)
+sortedData = sorted(data, key=lambda tl: tl['created_at'],reverse=True)
+topData = sortedData[:10]
+map(f, topData)
+
+print(sortedData[0]['text'])
+
+#records = [json.loads(line) for line in input]
+#get the last 10 max
+#for each record, get its sentement
 
 
 
