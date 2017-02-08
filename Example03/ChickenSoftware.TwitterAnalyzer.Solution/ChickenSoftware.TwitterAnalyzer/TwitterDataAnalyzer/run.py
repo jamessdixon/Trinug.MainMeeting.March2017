5fa4ef2911db7c16309ed87248753d8f2d14adaf
Jamie Dixon <jamessdixon@gmail.com>
2017-02-07 21:47:44 -0500

    updated python

--- Example03/ChickenSoftware.TwitterAnalyzer.Solution/ChickenSoftware.TwitterAnalyzer/TwitterDataAnalyzer/run.py
+++ Example03/ChickenSoftware.TwitterAnalyzer.Solution/ChickenSoftware.TwitterAnalyzer/TwitterDataAnalyzer/run.py
@@ -1,7 +1,34 @@
-﻿import os
-import json
+﻿#https://text-analytics-demo.azurewebsites.net/
+#https://westus.dev.cognitive.microsoft.com/docs/services/TextAnalytics.V2.0/operations/56f30ceeeda5650db055a3c9
 
-input = open(os.environ['inTable']).read()
-records = [json.loads(line) for line in input]
+import httplib, urllib, base64, json
 
-print(input)
+headers = {
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '15ac8ef72e3c4458a90cbdb024c68466'
+}
+
+params = urllib.urlencode({})
+
+def getSentimentScore(text):
+    try:
+        body = '{"documents": [{"language": "en","id": "1","text": "' + text + '"}]}'
+        conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+        conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, body, headers)
+        response = conn.getresponse()
+        json_data = response.read()
+        conn.close()
+        data =  json.loads(json_data)
+        return float(data['documents'][0]['score'])
+    except Exception as e:
+        return 50.0
+    return
+
+
+#input = open(os.environ['inTable']).read()
+#records = [json.loads(line) for line in input]
+#get the last 10 max
+#for each record, get its sentement
+
+
+
--- Example03/ChickenSoftware.TwitterAnalyzer.Solution/NLPPoC/NLPPoC.py
+++ Example03/ChickenSoftware.TwitterAnalyzer.Solution/NLPPoC/NLPPoC.py
@@ -1,37 +1,30 @@
 
-import httplib, urllib, base64
+import httplib, urllib, base64, json
 
 headers = {
     'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': '{subscription key}',
+    'Ocp-Apim-Subscription-Key': '15ac8ef72e3c4458a90cbdb024c68466'
 }
 
-params = urllib.urlencode({
-})
+params = urllib.urlencode({})
 
-try:
-    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+def getSentimentScore(text):
+    try:
+        body = '{"documents": [{"language": "en","id": "1","text": "' + text + '"}]}'
+        conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+        conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, body, headers)
+        response = conn.getresponse()
+        json_data = response.read()
+        conn.close()
+        data =  json.loads(json_data)
+        return float(data['documents'][0]['score'])
+    except Exception as e:
+        return 50.0
+    return
 
-#import json
+print(getSentimentScore('This is awesome'))
+print(getSentimentScore('This sucks'))
 
-#path = 'TweetLite.json'
-#input = open(path).read()
-#obj = json.loads(input)
-#print(obj)
 
-#records = [json.loads(line) for line in open(path,'r')]
-#text = [rec['text'] for rec in records if 'text' in rec]
-
-#https://text-analytics-demo.azurewebsites.net/
-#https://westus.dev.cognitive.microsoft.com/docs/services/TextAnalytics.V2.0/operations/56f30ceeeda5650db055a3c9
-
-#print(records)
 
     
